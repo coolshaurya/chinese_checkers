5b6d97ec89548ea9330c49b2a74e43a64ede2251
Shaurya Shubham <shauryashubham1234567890@gmail.com>
2020-04-28 21:35:41 +0530

    update the game with players and stuff

--- src/board.rs
+++ src/board.rs
@@ -1,7 +1,8 @@
 use std::collections::{HashMap, HashSet};
+use std::ops;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum PlayerId {
+pub enum SideOfStar {
     A,
     B,
     C,
@@ -10,9 +11,11 @@ pub enum PlayerId {
     F,
 }
 
-impl PlayerId {
+pub type Player = SideOfStar;
+
+impl SideOfStar {
     fn forward(self) -> Self {
-        use PlayerId::*;
+        use SideOfStar::*;
         match self {
             A => B,
             B => C,
@@ -24,7 +27,7 @@ impl PlayerId {
     }
 
     fn opposite(self) -> Self {
-        use PlayerId::*;
+        use SideOfStar::*;
         match self {
             A => D,
             B => E,
@@ -36,13 +39,13 @@ impl PlayerId {
     }
 }
 
-impl Default for PlayerId {
+impl Default for SideOfStar {
     fn default() -> Self {
         Self::A
     }
 }
 
-impl Iterator for PlayerId {
+impl Iterator for SideOfStar {
     type Item = Self;
     fn next(&mut self) -> Option<Self::Item> {
         Some(self.forward())
@@ -52,7 +55,23 @@ impl Iterator for PlayerId {
 #[derive(Debug, Clone, Copy)]
 pub enum Spot {
     Empty,
-    Player(PlayerId),
+    Player(Player),
+}
+
+impl Spot {
+    pub fn is_empty(self) -> bool {
+        match self {
+            Self::Empty => true,
+            Self::Player(_) => false,
+        }
+    }
+
+    pub fn is_player(self, player: Player) -> bool {
+        match self {
+            Self::Player(current_player) => player == current_player,
+            Self::Empty => false,
+        }
+    }
 }
 
 impl Default for Spot {
@@ -69,9 +88,57 @@ pub struct HexCoord {
     pub slant: i32,
 }
 
+impl From<(i32, i32)> for HexCoord {
+    fn from(other: (i32, i32)) -> Self {
+        Self::new(other.0, other.1)
+    }
+}
+
+impl ops::Add for HexCoord {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Self::new(self.horz + other.horz, self.slant + other.slant)
+    }
+}
+
+impl ops::Mul<i32> for HexCoord {
+    type Output = Self;
+    fn mul(self, other: i32) -> Self {
+        Self::new(self.horz * other, self.slant * other)
+    }
+}
+
+impl ops::Neg for HexCoord {
+    type Output = Self;
+    fn neg(self) -> Self {
+        Self::new(-self.horz, -self.slant)
+    }
+}
+
+impl ops::Sub for HexCoord {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        self + -other
+    }
+}
+
 impl HexCoord {
-    const fn new(horz: i32, slant: i32) -> Self {
+    pub const fn new(horz: i32, slant: i32) -> Self {
         HexCoord { horz, slant }
+    }
+
+    fn neighbors(self) -> Vec<Self> {
+        vec![
+            Self::new(0, -1),
+            Self::new(0, 1),
+            Self::new(1, 0),
+            Self::new(-1, 0),
+            Self::new(-1, 1),
+            Self::new(1, -1),
+        ]
+        .into_iter()
+        .map(|neighbor_offset| self + neighbor_offset)
+        .collect()
     }
 
     fn triangle_tip_up(self, size: i32) -> Vec<Self> {
@@ -99,28 +166,87 @@ impl HexCoord {
     }
 }
 
-/*
 #[derive(Debug, Clone)]
-enum GameOutcome {
-    Indefinite,
-    WinSequence(Vec<PlayerId>),
-    Finished(Vec<PlayerId>),
-}*/
+enum Move {
+    Shift(HexCoord),
+    Jump(Vec<HexCoord>),
+}
+
+#[derive(Debug, Clone, Default)]
+struct GameOutcome(Vec<Player>);
 
 #[derive(Debug, Clone)]
 pub struct Board {
     pub board: HashMap<HexCoord, Spot>,
-    pub players: HashSet<PlayerId>,
-    pub turn: PlayerId,
+    pub players: HashSet<Player>,
+    pub turn: Player,
 }
 
 impl Board {
     pub fn new(players_count: usize) -> Self {
-        Self {
+        let mut new_board = Self {
             players: gen_players(players_count),
             board: gen_board(),
-            turn: PlayerId::default(),
+            turn: Player::default(),
+        };
+
+        new_board.setup_players();
+
+        new_board
+    }
+
+    fn setup_players(&mut self) {
+        let player_tip = |side_of_star: SideOfStar| -> Vec<HexCoord> {
+            use SideOfStar::*;
+            match side_of_star {
+                A => HexCoord::new(4, -8).triangle_tip_up(4),
+                B => HexCoord::new(5, -1).triangle_tip_down(4),
+                C => HexCoord::new(4, 1).triangle_tip_up(4),
+                D => HexCoord::new(-4, 8).triangle_tip_down(4),
+                E => HexCoord::new(-5, 1).triangle_tip_up(4),
+                F => HexCoord::new(-4, -1).triangle_tip_down(4),
+            }
+        };
+
+        for &player in self.players.clone().iter() {
+            let player_tip = player_tip(player);
+            for player_tip_coord in player_tip {
+                self.put_player(player_tip_coord, player);
+            }
         }
+    }
+
+    pub fn put_player(&mut self, coord: HexCoord, player: Player) {
+        if !self.is_valid(coord) {
+            return;
+        }
+        self.board.insert(coord, Spot::Player(player));
+    }
+
+    pub fn get(&self, coord: &HexCoord) -> Option<&Spot> {
+        self.board.get(coord)
+    }
+
+    pub fn remove_player(&mut self, coord: HexCoord) {
+        if !self.is_valid(coord) {
+            return;
+        }
+        self.board.insert(coord, Spot::Empty);
+    }
+
+    fn is_valid(&self, coord: HexCoord) -> bool {
+        self.board.contains_key(&coord)
+    }
+
+    pub fn swap(&mut self, coord1: HexCoord, coord2: HexCoord) {
+        if !self.is_valid(coord1) || !self.is_valid(coord2) {
+            return;
+        }
+        let spot1 = self.board.get(&coord1).unwrap().clone();
+        let spot2 = self.board.get(&coord2).unwrap().clone();
+
+        self.board.insert(coord1, spot2);
+        self.board.insert(coord2, spot1);
     }
 }
 
@@ -139,41 +265,18 @@ fn gen_board() -> HashMap<HexCoord, Spot> {
         .collect()
 }
 
-fn gen_players(players_count: usize) -> HashSet<PlayerId> {
+fn gen_players(players_count: usize) -> HashSet<Player> {
+    use SideOfStar::*;
     let players_count = match players_count {
         2 | 3 | 4 | 6 => players_count,
         _ => 2,
     };
 
-    let mut players = HashSet::with_capacity(6);
     match players_count {
-        2 => {
-            players = PlayerId::default()
-                .enumerate()
-                .take(4)
-                .filter(|(index, _)| index % 3 == 0)
-                .map(|(_, val)| val)
-                .collect();
-        }
-        3 => {
-            players = PlayerId::default()
-                .enumerate()
-                .take(5)
-                .filter(|(index, _)| index % 2 == 0)
-                .map(|(_, val)| val)
-                .collect();
-        }
-        4 => {
-            players = PlayerId::default()
-                .take(2)
-                .flat_map(|val| vec![val, val.opposite()])
-                .collect();
-        }
-        6 => {
-            players = PlayerId::default().take(6).collect();
-        }
+        2 => vec![A, D].into_iter().collect(),
+        3 => vec![A, C, E].into_iter().collect(),
+        4 => vec![A, B, D, E].into_iter().collect(),
+        6 => vec![A, B, C, D, E, F].into_iter().collect(),
         _ => unreachable!(),
     }
-
-    players
 }
--- src/gui.rs
+++ src/gui.rs
@@ -1,64 +1,166 @@
-use ggez::conf::NumSamples;
+use ggez::conf::{WindowMode, WindowSetup};
 use ggez::event::{self, EventHandler};
-use ggez::graphics::{self, mint, Canvas, DrawMode, DrawParam, MeshBuilder};
+use ggez::graphics::{self, mint::Point2, Color, DrawMode, DrawParam, Mesh, MeshBuilder};
+use ggez::input::mouse::{self, MouseButton, MouseCursor};
+use ggez::timer;
 use ggez::{Context, ContextBuilder, GameResult};
 
-use crate::board::{Board, HexCoord};
+use std::time::{Duration, Instant};
+
+use crate::board::{Board, HexCoord, Player, SideOfStar, Spot};
+
+const SIN_30_DEG: f32 = 0.5;
+const COS_30_DEG: f32 = 0.866025403784439;
+const SIDE: f32 = 25.0;
+
+impl Spot {
+    fn color(self) -> Color {
+        match self {
+            Self::Empty => Color::from_rgb_u32(0xEEEEEE),
+            Self::Player(player) => match player {
+                Player::A => Color::from_rgb_u32(0xEE1133),
+                Player::B => Color::from_rgb_u32(0xFFE122),
+                Player::C => Color::from_rgb_u32(0xEE22CC),
+                Player::D => Color::from_rgb_u32(0x22EE55),
+                Player::E => Color::from_rgb_u32(0x2255FF),
+                Player::F => Color::from_rgb_u32(0xAA22FF),
+            },
+        }
+    }
+}
 
 impl HexCoord {
-    fn to_point2(self, side: f32) -> mint::Point2<f32> {
-        const SIN_30_DEG: f32 = 0.5;
-        const COS_30_DEG: f32 = 0.866025403784439;
+    fn hexagon_center(self, side: f32) -> Point2<f32> {
         let horz: f32 = self.horz as f32;
         let slant: f32 = self.slant as f32;
         let x: f32 = side * COS_30_DEG * (2.0 * horz + slant);
         let y: f32 = slant * (side * SIN_30_DEG + side);
-        println!("({}, {})", x, y);
         [x, y].into()
+    }
+
+    // NOTE: all the code in this fn has been directly copied from
+    // https://www.redblobgames.com/grids/hexagons/ it uses the
+    // pixel to hex conversion and then rounds it. In the rounding
+    // code the redundant `rz` has been removed where nescessary
+    fn from_point(point: Point2<f32>, side: f32) -> Self {
+        let point = Point2::from([point.x - 350.0, point.y - 350.0]);
+
+        let horz = (3.0_f32.sqrt() / 3.0 * point.x - 1.0 / 3.0 * point.y) / side;
+        let slant = (2.0 / 3.0 * point.y) / side;
+
+        let x = horz;
+        let y = slant;
+        let z = -horz - slant;
+
+        let mut rx = x.round();
+        let mut ry = y.round();
+        let rz = z.round();
+
+        let x_diff = (rx - x).abs();
+        let y_diff = (ry - y).abs();
+        let z_diff = (rz - z).abs();
+
+        if x_diff > y_diff && x_diff > z_diff {
+            rx = -ry - rz
+        } else if y_diff > z_diff {
+            ry = -rx - rz
+        }
+
+        Self::new(rx as i32, ry as i32)
     }
 }
 
+fn ideal_radius(hexagon_side: f32) -> f32 {
+    hexagon_side * COS_30_DEG * 0.97
+}
+
 #[derive(Clone, Debug)]
-struct BoardState {
+struct BoardGui {
     board: Board,
 }
 
-impl BoardState {
+impl BoardGui {
     fn new() -> Self {
         Self {
             board: Board::new(2),
         }
     }
+
+    fn circle_mesh(&self, context: &mut Context) -> GameResult<Mesh> {
+        let circle_centers = self
+            .board
+            .board
+            .iter()
+            .map(|(&coord, &spot)| (coord.hexagon_center(SIDE), spot.color()));
+
+        let mut mesh = MeshBuilder::new();
+
+        for (circle_center, color) in circle_centers {
+            mesh.circle(
+                DrawMode::fill(),
+                circle_center,
+                ideal_radius(SIDE),
+                0.1,
+                color,
+            );
+        }
+
+        mesh.build(context)
+    }
 }
 
-impl EventHandler for BoardState {
-    fn update(&mut self, context: &mut Context) -> GameResult {
+impl EventHandler for BoardGui {
+    fn update(&mut self, _context: &mut Context) -> GameResult {
         Ok(())
     }
     fn draw(&mut self, context: &mut Context) -> GameResult {
         graphics::clear(context, graphics::BLACK);
 
-        let circle_centers = self.board.board.keys().map(|&coord| coord.to_point2(22.0));
-        let mut mesh = MeshBuilder::new();
+        let highlighted = HexCoord::from_point(mouse::position(context), SIDE);
 
-        for circle_center in circle_centers {
-            mesh.circle(DrawMode::fill(), circle_center, 18.0, 0.1, graphics::WHITE);
+        if mouse::button_pressed(context, MouseButton::Left) {
+            mouse::set_cursor_type(context, MouseCursor::Grabbing);
+        } else {
+            mouse::set_cursor_type(context, MouseCursor::Default);
         }
 
-        let mesh = mesh.build(context)?;
+        let circles = self.circle_mesh(context)?;
 
-        graphics::draw(context, &mesh, DrawParam::default().dest([300.0, 300.0]))?;
+        graphics::draw(context, &circles, DrawParam::default().dest([350.0, 350.0]))?;
 
         graphics::present(context)?;
+        timer::yield_now();
 
         Ok(())
+    }
+
+    fn mouse_button_down_event(
+        &mut self,
+        _context: &mut Context,
+        _button: MouseButton,
+        x: f32,
+        y: f32,
+    ) {
+        self.last_click = Some(Instant::now());
+        println!("mouse pressed!");
+    }
+    fn mouse_button_up_event(
+        &mut self,
+        _context: &mut Context,
+        _button: MouseButton,
+        _x: f32,
+        _y: f32,
+    ) {
+        println!("mouse released!");
     }
 }
 
 pub fn start_game() -> GameResult {
-    let (mut context, mut events_loop) =
-        ContextBuilder::new("chinese_checkers", "coolshaurya").build()?;
-    event::run(&mut context, &mut events_loop, &mut BoardState::new())?;
+    let (mut context, mut events_loop) = ContextBuilder::new("chinese_checkers", "coolshaurya")
+        .window_setup(WindowSetup::default().title("Chinese Checkers"))
+        .window_mode(WindowMode::default().dimensions(700.0, 700.0))
+        .build()?;
+    event::run(&mut context, &mut events_loop, &mut BoardGui::new())?;
 
     Ok(())
 }
